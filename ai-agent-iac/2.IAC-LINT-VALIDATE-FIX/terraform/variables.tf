837a88c61c1ab1da94ee3d03dc1aee8470b017c2
shan5a6 <shahan.aix@gmail.com>
2025-09-28 07:13:03 +0000

    feat: enhanced lint,format,explain

---
+++ ai-agent-iac/2.IAC-LINT-VALIDATE-FIX/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "ami_id" {
+  type    = string
+  default = "ami-0af11aec6e4f10de7"
+}
+
+variable "key_name" {
+  type    = string
+  default = "MCB4KEY"
+}
